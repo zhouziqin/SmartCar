47b238d253015dcfab62d72821b0034d43a5efcf
zhouzq <zhouzq@deepblueai.com>
2020-07-16 15:49:26 +0800

    test

--- giant-ads/ads-common/src/main/java/com/deepblueai/ads/common/exception/AdsAlgException.java
+++
@@ -1,7 +0,0 @@
-package com.deepblueai.ads.common.exception;
-
-public class AdsAlgException extends Exception {
-    public AdsAlgException(String message){
-        super(message);
-    }
-}
--- giant-ads/ads-pos-service/src/main/java/com/deepblueai/ads/pos/service/BaseConfigService.java
+++
@@ -1,7 +0,0 @@
-package com.deepblueai.ads.pos.service;
-
-public interface BaseConfigService {
-
-  String getConfigValue(String configGroup, String configKey);
-
-}
